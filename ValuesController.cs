c1f09ce0e10b26818880edc251ae2a1e62333862
Tony Butler <tony-a.butler@ubs.com>
2016-04-11 14:33:13 +0100

    Initial Commit

---
+++ ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using WebApiContrib.ModelBinders;
+
+namespace MvcStyleBindingProblem
+{
+    [MvcStyleBinding]
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            Console.WriteLine("Get");
+            return new[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string GetById(int id)
+        {
+            Console.WriteLine("GetById: " + id);
+            return "value";
+        }
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+            Console.WriteLine("Post: " + value);
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+            Console.WriteLine("Put: " + id);
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+            Console.WriteLine("Delete: " + id);
+        }
+    }
+}
